47ea885d2a1b40e0b0df7c54add2b05288897fc4
galdino_mateus <galdino_mateus1000@outlook.com>
2021-03-10 13:30:05 -0300

    Updated project according to feedback and Project A11Y's guidelines to accessbility on the web.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const btnToggle = document.querySelector(".share-button");
 const shareImg = document.querySelector(".share-img");
 const shareBar = document.querySelector(".social-media-container");
 
+btnToggle.addEventListener("click", toggleSocialBar);
+
 function toggleSocialBar() {
   btnToggle.classList.toggle("share-button--active");
   shareImg.classList.toggle("share-img--active");
